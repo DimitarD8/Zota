16eb80e8a1a2bef4dda56e58173cf445652ad53a
Dimitar <7519538426>
2025-08-15 15:05:14 +0300

    Key value store base structure

---
+++ model/store.go
@@ -0,0 +1,22 @@
+package model
+
+import ()
+
+type Store struct {
+	data map[string]string
+}
+
+func NewStore() *Store {
+	return &Store{data: make(map[string]string)}
+}
+func (s *Store) Set(key, value string) {
+	s.data[key] = value
+}
+
+func (s *Store) Get(key string) string {
+	return s.data[key]
+}
+
+func (s *Store) Delete(key string) {
+	delete(s.data, key)
+}
