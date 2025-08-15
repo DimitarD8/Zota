d59fcec6e1d8f033467c29a9788188264cdca763
Dimitar <7519538426>
2025-08-15 17:29:39 +0300

    add the 30% chance

--- model/store.go
+++ model/store.go
@@ -17,6 +17,9 @@ func NewStore(db *pgxpool.Pool) *Store {
 	return &Store{data: make(map[string]string), db: db}
 }
 func (s *Store) Set(key, value string) error {
+	if isUnlucky() {
+		return nil
+	}
 	_, err := s.db.Exec(context.Background(),
 		"INSERT INTO store (key, value) VALUES ($1, $2) ON CONFLICT (key) DO NOTHING",
 		key, value)
@@ -24,6 +27,14 @@ func (s *Store) Set(key, value string) error {
 }
 
 func (s *Store) Get(key string) (string, error) {
+	if isUnlucky() {
+		var randomValue string
+		err := s.db.QueryRow(context.Background(), "SELECT value FROM store ORDER BY RANDOM() LIMIT 1", key).Scan(&randomValue)
+		if err != nil {
+			return "", err
+		}
+		return randomValue, nil
+	}
 	var value string
 	err := s.db.QueryRow(context.Background(), "SELECT value FROM store WHERE key = $1", key).Scan(&value)
 	if err != nil {
@@ -34,6 +45,12 @@ func (s *Store) Get(key string) (string, error) {
 }
 
 func (s *Store) Delete(key string) error {
+
+	if isUnlucky() {
+		_, err := s.db.Exec(context.Background(), "DELETE FROM store WHERE key IN (SELECT key FROM store ORDER BY random() LIMIT 1)", key)
+		return err
+	}
+
 	affected, err := s.db.Exec(context.Background(), "DELETE FROM store WHERE key = $1", key)
 	if err != nil {
 		return err
@@ -48,6 +65,6 @@ func (s *Store) Delete(key string) error {
 	return nil
 }
 
-func isLucky() bool {
+func isUnlucky() bool {
 	return rand.Intn(100) < 30
 }
