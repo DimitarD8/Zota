d93fffc34daf32d2819dccdb71ed9bdbe350fb36
Dimitar Dimitrov <98160494+DimitarD8@users.noreply.github.com>
2025-08-18 17:43:04 +0300

    more tests

--- store/store_test.go
+++ store/store_test.go
@@ -2,6 +2,7 @@ package store
 
 import (
 	"errors"
+	"regexp"
 	"testing"
 
 	"github.com/pashagolub/pgxmock"
@@ -47,6 +48,21 @@ func TestStore_Put_Error(t *testing.T) {
 	require.EqualError(t, err, "error")
 }
 
+func TestStore_Put_Unlucky(t *testing.T) {
+	mock, err := pgxmock.NewPool()
+	require.NoError(t, err)
+	defer mock.Close()
+
+	isUnlucky = func() bool { return true }
+	defer func() { isUnlucky = func() bool { return false } }()
+
+	store := NewStore(mock)
+
+	err = store.Put("foo", "bar")
+	require.NoError(t, err)
+	require.NoError(t, mock.ExpectationsWereMet())
+}
+
 func TestStore_Delete(t *testing.T) {
 	mock, err := pgxmock.NewPool()
 	require.NoError(t, err)
@@ -80,6 +96,27 @@ func TestStore_Delete_Error(t *testing.T) {
 	store := NewStore(mock)
 	err = store.Delete("foo")
 	require.EqualError(t, err, "error")
+}
+
+func TestStore_Delete_NoSuchKey(t *testing.T) {
+	mock, err := pgxmock.NewPool()
+	require.NoError(t, err)
+	defer mock.Close()
+
+	originalIsUnlucky := isUnlucky
+	isUnlucky = func() bool { return false }
+	defer func() { isUnlucky = originalIsUnlucky }()
+
+	mock.ExpectExec("DELETE FROM store").
+		WithArgs("foo").
+		WillReturnResult(pgxmock.NewResult("DELETE", 0))
+
+	store := NewStore(mock)
+	err = store.Delete("foo")
+
+	require.Error(t, err)
+	require.EqualError(t, err, "No such key: foo")
+	require.NoError(t, mock.ExpectationsWereMet())
 }
 
 func TestStore_Get(t *testing.T) {
@@ -167,4 +204,27 @@ func TestStore_Dumb_Error(t *testing.T) {
 
 	require.EqualError(t, err, "query failed: error")
 	require.Error(t, err)
+}
+
+func TestStore_Dumb_ScanError(t *testing.T) {
+	mock, err := pgxmock.NewPool()
+	require.NoError(t, err)
+	defer mock.Close()
+
+	originalIsUnlucky := isUnlucky
+	isUnlucky = func() bool { return false }
+	defer func() { isUnlucky = originalIsUnlucky }()
+
+	rows := pgxmock.NewRows([]string{"wrong"}).
+		AddRow("oops")
+
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT key, value FROM store")).
+		WillReturnRows(rows)
+
+	store := NewStore(mock)
+	_, err = store.Dumb()
+
+	require.Error(t, err)
+	require.Contains(t, err.Error(), "error scanning row")
+	require.NoError(t, mock.ExpectationsWereMet())
 }
